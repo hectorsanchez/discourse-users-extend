3ff8c399ca40972f37bd4e9882cb61e1a9d00084
hectorsanchez <hectorsanchez.mov@gmail.com>
2025-09-22 17:32:55 -0300

    Move routes back to plugin.rb and remove separate routes file!

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Discourse::Application.routes.append do
-  get '/discourse/users' => 'discourse_users#index'
-  get '/discourse/users/api' => 'discourse_users#users'
-  get '/discourse/debug' => 'discourse_users#debug'
-  get '/discourse/test' => 'discourse_users#test'
-  post '/discourse/save_settings' => 'discourse_users#save_settings'
-end
