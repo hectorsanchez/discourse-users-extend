74a7494c1ce0409c3fefd8c2ced5e9f899016880
hectorsanchez <hectorsanchez.mov@gmail.com>
2025-09-24 14:35:32 -0300

    Fix country extraction by using individual user API calls

--- plugin.rb
+++ plugin.rb
@@ -26,47 +26,68 @@ after_initialize do
       end
 
       begin
-        # Use multiple directory calls to get more users and better country coverage
-        # Directory endpoint is limited to 50 users per call, so we need multiple calls
-        all_users = []
+        # Use directory endpoint to get usernames, then fetch individual user data
+        # Directory endpoint doesn't include location field, so we need individual calls
+        directory_url = "#{discourse_url}/directory_items.json?order=created&period=all&asc=true"
         
-        # Get users from different time periods to get better coverage
-        periods = ['all', 'yearly', 'monthly', 'weekly', 'daily']
+        require 'net/http'
+        require 'uri'
+        require 'json'
         
-        periods.each do |period|
-          directory_url = "#{discourse_url}/directory_items.json?order=created&period=#{period}&asc=true"
+        uri = URI(directory_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == 'https'
+        http.read_timeout = 30
         
-          # Make request for this period
-          require 'net/http'
-          require 'uri'
-          require 'json'
+        request = Net::HTTP::Get.new(uri)
+        request['Api-Key'] = api_key
+        request['Api-Username'] = api_username
+        
+        directory_response = http.request(request)
+        
+        if directory_response.code.to_i == 200
+          directory_data = JSON.parse(directory_response.body)
+          users = directory_data['directory_items'] || []
           
-          uri = URI(directory_url)
-          http = Net::HTTP.new(uri.host, uri.port)
-          http.use_ssl = true if uri.scheme == 'https'
-          http.read_timeout = 30
+          Rails.logger.info "Got #{users.length} users from directory"
           
-          request = Net::HTTP::Get.new(uri)
-          request['Api-Key'] = api_key
-          request['Api-Username'] = api_username
-          
-          directory_response = http.request(request)
-          
-          if directory_response.code.to_i == 200
-            directory_data = JSON.parse(directory_response.body)
-            users = directory_data['directory_items'] || []
-            all_users.concat(users)
-            Rails.logger.info "Period #{period}: #{users.length} users"
-          else
-            Rails.logger.warn "Failed to get users for period #{period}: #{directory_response.code}"
+          # Get individual user data to access location field
+          unique_users = []
+          users.each_with_index do |user_item, index|
+            begin
+              username = user_item['user']['username']
+              
+              # Get individual user data
+              user_url = "#{discourse_url}/users/#{username}.json"
+              user_uri = URI(user_url)
+              user_http = Net::HTTP.new(user_uri.host, user_uri.port)
+              user_http.use_ssl = true if user_uri.scheme == 'https'
+              user_http.read_timeout = 30
+              
+              user_request = Net::HTTP::Get.new(user_uri)
+              user_request['Api-Key'] = api_key
+              user_request['Api-Username'] = api_username
+              
+              user_response = user_http.request(user_request)
+              
+              if user_response.code.to_i == 200
+                user_data = JSON.parse(user_response.body)['user']
+                unique_users << { 'user' => user_data }
+                Rails.logger.info "User #{index + 1}/#{users.length}: #{username} - location: '#{user_data['location']}'"
+              else
+                Rails.logger.warn "Failed to get user #{username}: #{user_response.code}"
+              end
+              
+              # Small delay to avoid rate limiting
+              sleep(0.1)
+            rescue => e
+              Rails.logger.error "Error processing user #{username}: #{e.message}"
+            end
           end
-          
-          # Small delay to avoid rate limiting
-          sleep(0.5)
+        else
+          Rails.logger.error "Failed to get directory: #{directory_response.code}"
+          unique_users = []
         end
-        
-        # Remove duplicates based on username
-        unique_users = all_users.uniq { |u| u['user']['username'] }
         
         Rails.logger.info "=== DISCOURSE USERS DEBUG ==="
         Rails.logger.info "Total unique users collected: #{unique_users.length}"
@@ -82,55 +103,32 @@ after_initialize do
           Rails.logger.info "Sample user data: #{sample_user.inspect}"
         end
         
-        # Try to extract country from different possible fields
+        # Extract countries from location field
         unique_users.each do |user_item|
           begin
             user_data = user_item['user']
-            
-            # Check multiple possible location fields
-            location = user_data['location'] || user_data['user_location'] || user_data['geo_location'] || ""
-            bio = user_data['bio_raw'] || user_data['bio'] || ""
-            name = user_data['name'] || ""
-            
-            Rails.logger.info "User #{user_data['username']}:"
-            Rails.logger.info "  - location: '#{location}'"
-            Rails.logger.info "  - bio: '#{bio[0..100]}...'" if bio.present?
-            Rails.logger.info "  - name: '#{name}'"
-            
-            # Try to extract country from location field
-            country = nil
-            if location.present?
-              if location.include?(',')
-                country = location.split(',').last.strip
-              else
-                country = location.strip
+                location = user_data['location'] || ""
+                
+            Rails.logger.info "User #{user_data['username']}: location='#{location}'"
+                
+            # Extract country from location
+                if location.present?
+                  if location.include?(',')
+                    country = location.split(',').last.strip
+                  else
+                    country = location.strip
               end
-            end
-            
-            # If no country from location, try to extract from bio or name
-            if country.blank? && bio.present?
-              # Look for country names in bio
-              country_patterns = [
-                /(?:from|in|based in|located in)\s+([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*)/i,
-                /([A-Z][a-z]+(?:\s+[A-Z][a-z]+)*)\s+(?:resident|citizen|national)/i
-              ]
               
-              country_patterns.each do |pattern|
-                match = bio.match(pattern)
-                if match
-                  country = match[1].strip
-                  break
-                end
+              Rails.logger.info "  -> Extracted country: '#{country}'"
+              
+              if country.present? && country != "No country" && country.length > 2
+                countries_set.add(country)
+                Rails.logger.info "  -> Added to countries set: '#{country}'"
+              else
+                Rails.logger.info "  -> Skipped country: '#{country}' (empty, too short, or 'No country')"
               end
-            end
-            
-            Rails.logger.info "  -> Extracted country: '#{country}'"
-            
-            if country.present? && country != "No country" && country.length > 2
-              countries_set.add(country)
-              Rails.logger.info "  -> Added to countries set: '#{country}'"
             else
-              Rails.logger.info "  -> Skipped country: '#{country}' (empty, too short, or 'No country')"
+              Rails.logger.info "  -> No location data"
             end
           rescue => e
             Rails.logger.error "Error processing user #{user_item['user']['username']}: #{e.message}"
@@ -236,9 +234,9 @@ after_initialize do
                   user_country = location.split(',').last.strip
                 else
                   user_country = location.strip
+                  end
                 end
-              end
-              
+                
               # Only process if country matches
               if user_country == country
                 # Split name safely
@@ -268,7 +266,7 @@ after_initialize do
           render json: { 
             success: true, 
             users: processed_users,
-            country: country,
+                country: country,
             total_users: processed_users.length,
             timestamp: Time.current.iso8601
           }
